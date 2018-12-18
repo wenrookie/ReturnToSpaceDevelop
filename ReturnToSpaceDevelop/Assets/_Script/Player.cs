8823ab889918d2224a297173d0d935ea6fbb2650
sharp98 <1529843332@qq.con>
2018-12-18 15:00:19 +0800

    newrename

---
+++ ReturnToSpaceDevelop/Assets/_Script/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    #region Private Variable
+
+    #endregion
+
+    #region Private Method
+
+    #endregion
+
+    #region Protected Variable
+
+    #endregion
+
+    #region Protected Method
+
+    #endregion
+
+    #region Public Variable
+
+    #endregion
+
+    #region Public Property
+
+    #endregion
+
+    #region Public Method
+
+    #endregion
+}
